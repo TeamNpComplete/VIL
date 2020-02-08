34dbff7c4de3569087fd3aafd34d956b45c4ad12
imperial-dragon <Marktwist@1365>
2020-02-08 17:02:48 +0530

    DailoFlow Integration

--- app/src/main/java/com/vil/vil_bot/MainActivity.java
+++ app/src/main/java/com/vil/vil_bot/MainActivity.java
@@ -1,14 +1,126 @@
 package com.vil.vil_bot;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.gson.JsonElement;
+
+import java.util.Map;
+
+import ai.api.AIListener;
+import ai.api.AIServiceException;
+import ai.api.android.AIConfiguration;
+import ai.api.android.AIService;
+import ai.api.model.AIError;
+import ai.api.model.AIRequest;
+import ai.api.model.AIResponse;
+import ai.api.model.Result;
+
+public class MainActivity extends AppCompatActivity implements AIListener {
+
+    AIService aiService;
+    TextView t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        t = findViewById(R.id.textView);
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+
+            makeRequest();
+        }
+        final AIConfiguration config = new AIConfiguration("555542b49b8b456eb79774954ae64f1e",
+                AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.RecognitionEngine.System);
+        aiService = AIService.getService(this, config);
+        aiService.setListener(this);
+    }
+
+    protected void makeRequest() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.RECORD_AUDIO},
+                101);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 101: {
+
+                if (grantResults.length == 0
+                        || grantResults[0] !=
+                        PackageManager.PERMISSION_GRANTED) {
+
+
+                } else {
+
+                }
+                return;
+            }
+        }
+    }
+
+
+    public void buttonClicked(View view) throws AIServiceException {
+        aiService.startListening();
+        //AIRequest aiRequest = new AIRequest("Hello");
+        //aiService.textRequest(aiRequest);
+    }
+
+    @Override
+    public void onResult(AIResponse result) {
+
+        Log.d("anu",result.toString());
+        Result result1=result.getResult();
+
+        String parameterString = "";
+
+        if (result1.getParameters() != null && !result1.getParameters().isEmpty()) {
+            for (final Map.Entry<String, JsonElement> entry : result1.getParameters().entrySet()) {
+                parameterString += "(" + entry.getKey() + ", " + entry.getValue() + ") ";
+            }
+        }
+
+        t.setText("Query "+result1.getResolvedQuery()+" action: "+parameterString);
+
+
+    }
+
+    @Override
+    public void onError(AIError error) {
+
+    }
+
+    @Override
+    public void onAudioLevel(float level) {
+
+    }
+
+    @Override
+    public void onListeningStarted() {
+
+    }
+
+    @Override
+    public void onListeningCanceled() {
+
+    }
+
+    @Override
+    public void onListeningFinished() {
+
     }
 }
